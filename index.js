d3f190232ddd3eca5c48f9857f105924e3a5f317
alwysaid <alwysaid@icloud.com>
2024-04-27 19:57:12 +0700

    catatanfd3

---
+++ index.js
@@ -0,0 +1,46 @@
+
+//let firstName = "Witro" //contoh reassign
+//firstName = "Rifqi"
+
+//console.log(firstName, "==> BIAR TIDAK DA DUSTA DIANTARA KITA");
+
+myCar = "Esteem"
+var myCar
+
+console.log(myCar, "==> Hasilnya apa");
+
+
+/* 
+
+scope : pembatasnya curly bracket {}
+
+ex : let Firstname {"Alwy"}
+
+Reassign : valuenya dapat dimasukkan/diganti
+
+ex :    let firstName = "Witro"
+        firstName = "Rifqi"
+
+Redeclared : mirip kayak reassign 
+
+ex : let juiceMenus = "Juice Papuket"
+
+
+const dessertMenus = "Puding pudong"
+
+hoisting : variabel sudah di deklarasikan tapi belum memiliki nilai
+
+ex : 
+
+myCar = "Esteem"
+var myCar
+
+console.log(myCar, "==> Hasilnya apa");
+
+Data : Primitif dan Non primitif
+
+Non primitif : Object , Array
+
+ekspresi : Sesuatu yang mengandung nilai
+
+*/
